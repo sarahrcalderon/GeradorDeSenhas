19c49acd3e49de86a342561f36329b02a16f8288
sarahrcalderon <srcalderonn@gmail.com>
2025-01-24 19:24:25 -0300

    verifica segurança da senha

--- gerador_senhas.sh
+++ gerador_senhas.sh
@@ -2,8 +2,32 @@ echo "Esse é o Gerador de Senhas Aleatórias!"
 echo "Quantos caracteres a senha deve ter?"
 read caracteres
 
+
 senha=$(openssl rand -base64 48 | cut -c1-$caracteres)
 echo "Sua senha gerada é: $senha"
+
+
+function verificar_forca() {
+  local senha="$1"
+  local forca=0
+
+  # Verifica se contém letras maiúsculas, minúsculas, números e símbolos
+  [[ "$senha" =~ [A-Z] ]] && ((forca++))
+  [[ "$senha" =~ [a-z] ]] && ((forca++))
+  [[ "$senha" =~ [0-9] ]] && ((forca++))
+ 
+  [[ "$senha" =~ [\!\@\#\$\%\^\&\*\(\)\_\+\=\-] ]] && ((forca++))
+
+  case $forca in
+    4) echo "Senha forte!" ;;
+    3) echo "Senha moderada." ;;
+    2) echo "Senha fraca." ;;
+    *) echo "Senha muito fraca!" ;;
+  esac
+}
+
+verificar_forca "$senha"
+
 
 echo "Deseja salvar a senha em um arquivo? (s/n)"
 read salvar
